769a3af25648db79912508bb90a5c2b0e71b9503
Pikosha2101 <Puka4yBTanke@gmail.com>
2023-11-14 05:12:34 +0300

    авторизация готова

--- app/src/main/java/com/example/test/fragments/AuthorizationFragment.kt
+++ app/src/main/java/com/example/test/fragments/AuthorizationFragment.kt
@@ -1,6 +1,5 @@
 package com.example.test.fragments
 
-import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -13,6 +12,10 @@ import com.example.test.databinding.AuthorizationFragmentBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class AuthorizationFragment : Fragment(R.layout.authorization_fragment) {
     private var _binding: AuthorizationFragmentBinding? = null
@@ -33,37 +36,30 @@ class AuthorizationFragment : Fragment(R.layout.authorization_fragment) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) = with(binding) {
         button.setOnClickListener{
-            if (editTextTextEmailAddress.text.isEmpty() || editTextTextPassword.text.isEmpty()){
-                Toast.makeText(requireContext(), "Введите данные во все поля!", Toast.LENGTH_LONG).show()
-            } else {
-                /*val coroutineScope = CoroutineScope(Dispatchers.Main)
-                coroutineScope.launch {
-                    val database = AppDatabase.getInstance(requireContext())
-                    val agentDao = database.agentDao()
-                    val cnt: Int = agentDao.getAgentCheck(editTextTextEmailAddress.text.toString(), editTextTextPassword.text.toString())
-                    if (cnt == 1) {
-                        val username = editTextTextEmailAddress.text.toString()
-                        val sharedPreferences = requireContext().getSharedPreferences(PREFS_NAME, 0)
-                        //получение кол-ва заходов у текущего юзера
-                        val currentCount = sharedPreferences.getInt(username + KEY_LOGIN_COUNT, 0)
-                        //это кол-во + 1
-                        sharedPreferences.edit().putInt(username + KEY_LOGIN_COUNT, currentCount + 1).apply()
+            CoroutineScope(Dispatchers.Main).launch {
+                /*try {*/
+                    val interceptor = HttpLoggingInterceptor()
+                    interceptor.level = HttpLoggingInterceptor.Level.BODY
 
-                        //самый юзер
-                        val mostLoggedInUser = sharedPreferences.getString(KEY_MOST_LOGGED_IN_USER, "")
-                        //его кол-во
-                        val mostLoggedInCount = sharedPreferences.getInt(mostLoggedInUser + KEY_LOGIN_COUNT, 0)
-                        // Если самая часто авторизующаяся учетка - обновление ее
-                        if (currentCount >= mostLoggedInCount) {
-                            sharedPreferences.edit().putString(KEY_MOST_LOGGED_IN_USER, username).apply()
-                        }
+                    val client = OkHttpClient.Builder()
+                        .addInterceptor(interceptor)
+                        .build()
+
+                    val retrofit = Retrofit.Builder()
+                        .baseUrl("http://82.146.37.164:8090/")
+                        .client(client)
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .build()
+
+                    val jsonAPI = retrofit.create(JsonAPI::class.java)
+
+                    val response = jsonAPI.authorizationByPass(emailEditText.text.toString(), passwordEditText.text.toString())
+
+                    if (response.isSuccessful) {
                         findNavController().navigate(R.id.action_authorizationFragment_to_firstFragment)
-                    }else {
-                        Toast.makeText(requireContext(), "Пользователь не существует!", Toast.LENGTH_LONG).show()
+                    } else {
+                        Toast.makeText(requireContext(), "Возникла ошибка", Toast.LENGTH_SHORT).show()
                     }
-
-                }*/
-                findNavController().navigate(R.id.action_authorizationFragment_to_firstFragment)
             }
         }
         button2.setOnClickListener{
--- app/src/main/java/com/example/test/fragments/JsonAPI.kt
+++ app/src/main/java/com/example/test/fragments/JsonAPI.kt
@@ -1,12 +1,19 @@
 package com.example.test.fragments
 
+import com.example.test.TokenModel
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
-import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface JsonAPI {
     @POST("user/register")
     suspend fun registrationByPass(@Body userModel: UserModel?) : Response<UserModel>
+
+    @GET("user/login")
+    suspend fun authorizationByPass(
+        @Query("email") email: String,
+        @Query("password") password: String
+    ) : Response<TokenModel>
 }
